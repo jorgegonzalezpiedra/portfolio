faeb751eddda22c79b5b5dbdebc5f18cd143de32
Jorge González Piedra <jorge.gonzalez.piedra@gmail.com>
2024-04-18 17:42:37 +0200

    Added hover style to cards

--- js/main.js
+++ js/main.js
@@ -2,20 +2,20 @@ const $heroSectionSkills = document.getElementById("hero-section-skills");
 const $heroSkillsContainer1 = document.getElementById("hero-skills-container-1");
 const $heroSkillsContainer2 = document.getElementById("hero-skills-container-2");
 
-$heroSectionSkills.addEventListener("mouseover", hoverSkills);
-$heroSectionSkills.addEventListener("mouseout", hoverSkills);
+// $heroSectionSkills.addEventListener("mouseover", hoverSkills);
+// $heroSectionSkills.addEventListener("mouseout", hoverSkills);
 
-function hoverSkills() {
-  toogleDisplay($heroSkillsContainer1);
-  toogleDisplay($heroSkillsContainer2);
-}
+// function hoverSkills() {
+//   toogleDisplay($heroSkillsContainer1);
+//   toogleDisplay($heroSkillsContainer2);
+// }
 
-function toogleDisplay(component) {
-  if (component.classList.contains('dp-none')) {
-    component.classList.remove('dp-none')
-    component.classList.add('dp-flex')
-  } else {
-    component.classList.remove('dp-flex')
-    component.classList.add('dp-none')
-  }
-}
+// function toogleDisplay(component) {
+//   if (component.classList.contains('dp-none')) {
+//     component.classList.remove('dp-none')
+//     component.classList.add('dp-flex')
+//   } else {
+//     component.classList.remove('dp-flex')
+//     component.classList.add('dp-none')
+//   }
+// }
