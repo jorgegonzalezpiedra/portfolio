76445a03882fd372713526e4ed5378ae8ea54a0c
Jorge Gonzalez <jorge.gonzalez.piedra@gmail.com>
2024-04-19 22:26:15 +0200

    Added 8bit speech bubbles TODO tags in  main section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+const aboutMeDescripctions = ["Hello I'm Jorge, I'm a developer with a few years of experience in my back (more than 6). Click to know more"
+, "Im a basketball player, and a sports enthusiastic",
+"I love programming, and make new things. I like to describe myself as a curious, competent, resolutive person"]
+let aboutMeCounter = 1;
+
 const $heroSectionSkills = document.getElementById("hero-section-skills");
 const $heroSkillsContainer1 = document.getElementById("hero-skills-container-1");
 const $heroSkillsContainer2 = document.getElementById("hero-skills-container-2");
@@ -8,15 +13,19 @@ const $heroMainContainer2 = document.getElementById("hero-main-container-2");
 
 const $heroMainAvatarNormal = document.getElementById("hero-main-avatar-normal");
 const $heroMainAvatarTalking = document.getElementById("hero-main-avatar-talking");
+const $heroMainBubble = document.getElementById("hero-main-bubble");
+const $heroMainAvatarContainer = document.getElementById("hero-main-avatar-container");
+$heroMainBubble.innerHTML = aboutMeDescripctions[0];
 
 $heroSectionSkills.addEventListener("mouseover", hoverSkills);
 $heroSectionSkills.addEventListener("mouseout", hoverSkills);
 
-$heroSectionMain.addEventListener("mouseover", hoverMain);
-$heroSectionMain.addEventListener("mouseout", hoverMain);
+// $heroSectionMain.addEventListener("mouseover", hoverMain);
+// $heroSectionMain.addEventListener("mouseout", hoverMain);
 
-$heroMainAvatarNormal.addEventListener("mouseover", hoverAvatar);
-$heroMainAvatarTalking.addEventListener("mouseout", hoverAvatar);
+$heroMainAvatarContainer.addEventListener("mouseover", hoverAvatar);
+$heroMainAvatarContainer.addEventListener("mouseout", hoverAvatar);
+$heroMainAvatarContainer.addEventListener("click", clickAvatar);
 
 
 function hoverSkills() {
@@ -32,6 +41,15 @@ function hoverMain() {
 function hoverAvatar() {
   toogleDisplay($heroMainAvatarNormal);
   toogleDisplay($heroMainAvatarTalking);
+}
+
+function clickAvatar() {
+  $heroMainBubble.innerHTML = aboutMeDescripctions[aboutMeCounter];
+  aboutMeCounter = aboutMeCounter + 1;
+  if(aboutMeCounter == aboutMeDescripctions.length){
+    aboutMeCounter = 0;
+  }
+  
 }
 
 function toogleDisplay(component) {
