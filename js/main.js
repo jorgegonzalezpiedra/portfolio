225459bc51f783dcec1cb4cc090cc547a1f0ee6d
Jorge Gonzalez <jorge.gonzalez.piedra@gmail.com>
2024-04-20 23:57:42 +0200

    Added functionality to experience box Added new svg icons

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ const aboutMeDescripctions = [
 "My career so far has been focused on <span style='color:#ff2288; font-weight:bold'>bakcend development</span> with <span style='color:#387ef0;'>Java, Spring and SQL</span> mainly",
 "But I'm not only that, when I'm not coding, you'll likely find me on the court <span style='font-style:italic'>shooting hoops</span> – a big basketball fan here! 🏀",
 "<span style='color:#ff2288; font-weight:bold;'>Learning</span> is my secret weapon. New languages, hot tech trends? Bring it on, I'm ready!",
-"So...If you have reached this point and you are up for a coding challenge or a hoop game? <span style='color:#387ef0; font-weight:bold'>Let's connect!</span> <span>You have my info in the box below ✌️</span>",
-"See ya! 👋"]
+"So...If you have reached this point and you are up for a coding challenge or a hoop game <span style='color:#387ef0; font-weight:bold'>Let's connect!</span>",
+"You have my <span style='color:#ff2288; font-weight:bold;'>info</span> in the <span style='font-style:italic'>contact section</span> below</br></br> See ya! 👋"]
 let aboutMeCounter = 0;
 
 const $heroSectionSkills = document.getElementById("hero-section-skills");
@@ -25,6 +25,10 @@ const $heroSectionAbouticons = document.getElementById("hero-section-abouticons"
 const $heroAbouticonsContainer1 = document.getElementById("hero-abouticons-container-1");
 const $heroAbouticonsContainer2 = document.getElementById("hero-abouticons-container-2");
 
+const $heroSectionExperience= document.getElementById("hero-section-experience");
+const $heroExperienceContainer1 = document.getElementById("hero-experience-container-1");
+const $heroExperienceContainer2 = document.getElementById("hero-experience-container-2");
+
 $heroSectionSkills.addEventListener("mouseover", hoverSkills);
 $heroSectionSkills.addEventListener("mouseout", hoverSkills);
 
@@ -34,8 +38,11 @@ $heroMainAvatarContainer.addEventListener("mouseover", hoverAvatar);
 $heroMainAvatarContainer.addEventListener("mouseout", hoverAvatar);
 $heroMainAvatarContainer.addEventListener("click", clickAvatar);
 
-// $heroSectionAbouticons.addEventListener("mouseover", hoverAboutIcons);
-// $heroSectionAbouticons.addEventListener("mouseout", hoverAboutIcons)
+$heroSectionAbouticons.addEventListener("mouseover", hoverAboutIcons);
+$heroSectionAbouticons.addEventListener("mouseout", hoverAboutIcons);
+
+// $heroSectionExperience.addEventListener("mouseover", hoverExperience);
+// $heroSectionExperience.addEventListener("mouseout", hoverExperience);
 
 function hoverSkills() {
     toogleDisplay($heroSkillsContainer1);
@@ -63,6 +70,11 @@ function clickAvatar() {
 function hoverAboutIcons() {
   toogleDisplay($heroAbouticonsContainer1);
   toogleDisplay($heroAbouticonsContainer2);
+}
+
+function hoverExperience(){
+  toogleDisplay($heroExperienceContainer1);
+  toogleDisplay($heroExperienceContainer2);
 }
 
 function toogleDisplay(component) {
