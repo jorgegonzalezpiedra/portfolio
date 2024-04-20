78205892de58aec15173b01fbcf50d9cc7472e7e
Jorge Gonzalez <jorge.gonzalez.piedra@gmail.com>
2024-04-20 13:25:37 +0200

    Added texts form main bubbles Added functionality to click on bubbles Added functionality to about section (TODO more icons and more buttons)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-const aboutMeDescripctions = ["Hello I'm Jorge, I'm a developer with a few years of experience in my back (more than 6). Click to know more"
-, "Im a basketball player, and a sports enthusiastic",
-"I love programming, and make new things. I like to describe myself as a curious, competent, resolutive person"]
-let aboutMeCounter = 1;
+const aboutMeDescripctions = [
+"Hey there! I'm Jorge, <span style='color:#ff2288; font-weight:bold; font-style: italic'>a developer</span>, which means I can fix your printer or you can <span style='color:#387ef0; font-weight:bold'>click here</span> to get to know me a little better",
+"My career so far has been focused on <span style='color:#ff2288; font-weight:bold'>bakcend development</span> with <span style='color:#387ef0;'>Java, Spring and SQL</span> mainly",
+"But I'm not only that, when I'm not coding, you'll likely find me on the court <span style='font-style:italic'>shooting hoops</span> – a big basketball fan here! 🏀",
+"<span style='color:#ff2288; font-weight:bold;'>Learning</span> is my secret weapon. New languages, hot tech trends? Bring it on, I'm ready!",
+"So...If you have reached this point and you are up for a coding challenge or a hoop game? <span style='color:#387ef0; font-weight:bold'>Let's connect!</span> <span>You have my info in the box below ✌️</span>",
+"See ya! 👋"]
+let aboutMeCounter = 0;
 
 const $heroSectionSkills = document.getElementById("hero-section-skills");
 const $heroSkillsContainer1 = document.getElementById("hero-skills-container-1");
@@ -15,18 +19,23 @@ const $heroMainAvatarNormal = document.getElementById("hero-main-avatar-normal")
 const $heroMainAvatarTalking = document.getElementById("hero-main-avatar-talking");
 const $heroMainBubble = document.getElementById("hero-main-bubble");
 const $heroMainAvatarContainer = document.getElementById("hero-main-avatar-container");
-$heroMainBubble.innerHTML = aboutMeDescripctions[0];
+$heroMainBubble.innerHTML = aboutMeDescripctions[aboutMeCounter];
+
+const $heroSectionAbouticons = document.getElementById("hero-section-abouticons");
+const $heroAbouticonsContainer1 = document.getElementById("hero-abouticons-container-1");
+const $heroAbouticonsContainer2 = document.getElementById("hero-abouticons-container-2");
 
 $heroSectionSkills.addEventListener("mouseover", hoverSkills);
 $heroSectionSkills.addEventListener("mouseout", hoverSkills);
 
-// $heroSectionMain.addEventListener("mouseover", hoverMain);
-// $heroSectionMain.addEventListener("mouseout", hoverMain);
-
+$heroSectionMain.addEventListener("mouseover", hoverMain);
+$heroSectionMain.addEventListener("mouseout", hoverMain);
 $heroMainAvatarContainer.addEventListener("mouseover", hoverAvatar);
 $heroMainAvatarContainer.addEventListener("mouseout", hoverAvatar);
 $heroMainAvatarContainer.addEventListener("click", clickAvatar);
 
+// $heroSectionAbouticons.addEventListener("mouseover", hoverAboutIcons);
+// $heroSectionAbouticons.addEventListener("mouseout", hoverAboutIcons)
 
 function hoverSkills() {
     toogleDisplay($heroSkillsContainer1);
@@ -44,12 +53,16 @@ function hoverAvatar() {
 }
 
 function clickAvatar() {
-  $heroMainBubble.innerHTML = aboutMeDescripctions[aboutMeCounter];
   aboutMeCounter = aboutMeCounter + 1;
-  if(aboutMeCounter == aboutMeDescripctions.length){
+  if(aboutMeCounter >= aboutMeDescripctions.length){
     aboutMeCounter = 0;
   }
-  
+  $heroMainBubble.innerHTML = aboutMeDescripctions[aboutMeCounter];
+}
+
+function hoverAboutIcons() {
+  toogleDisplay($heroAbouticonsContainer1);
+  toogleDisplay($heroAbouticonsContainer2);
 }
 
 function toogleDisplay(component) {
